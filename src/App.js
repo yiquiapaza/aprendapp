a9cca118cc98f335ee7ca28beca6f1c61d3d5be0
Yhonatan Jesus <yiquiapaza@gmail.com>
2020-11-12 20:19:52 -0300

    build: remove bulma style and add material-ui style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 const App = () => {
   return (
     <div>
+      <Button variant='contained' color='primary'>
+        {' '}
+        Hello world
+      </Button>
       <header>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        <a href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
           Learn React
         </a>
       </header>
